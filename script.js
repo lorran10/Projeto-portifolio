b8c031452c00994cb24e4022d46ba4b9ca6f7724
LoranOliveira <100364059+lorran10@users.noreply.github.com>
2022-03-12 13:21:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+<div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://emdiabetes.com.br/wp-content/uploads/2017/09/breve.jpg" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://emdiabetes.com.br/wp-content/uploads/2017/09/breve.jpg" alt="...">
+    </div>
+  
+  </div>
